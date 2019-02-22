db3b3ffcedbfd6feaae13e6d80000ad61264bd4f
pimpam <pimpamkanjana@gmail.com>
2019-02-22 09:53:50 +0700

    Microsoft cam test

--- blackdot_detect.py
+++ blackdot_detect.py
@@ -1,10 +1,11 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("hair_cut.png")
+img = cv2.imread("microsoft2.jpg")
 img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 lower_black = np.array([0,0,0])
-upper_black = np.array([180,255,180]) 
+
+upper_black = np.array([160,255,120]) 
 
 mask = cv2.inRange(img, lower_black, upper_black)
 cv2.imshow("Mask",mask)
