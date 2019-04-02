385565c42cc3145efbb09085a04840d5c2c35e81
pimpam <pimpamkanjana@gmail.com>
2019-04-02 10:37:09 +0700

    Changing

--- se.py
+++ se.py
@@ -1,5 +1,6 @@
 import serial 
 ser = serial.Serial('COM3')
+#ser = serial.Serial(port='COM3', baudrate=115200, parity=serial.PARITY_ODD, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
 print(ser.name)
 #print(ser.write(b'hello'))
 print(ser.write("!G 1 500".encode()))
